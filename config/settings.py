0daf8755de3993f78281a661e7b6109aeb182ed7
KirillKovardakov <kir.kovardakov@gmail.com>
2025-11-08 22:38:56 +0300

    deleted static and forms

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
